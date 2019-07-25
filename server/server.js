d0593662ec6049f30d4e979896189841e433bc17
Mike Curd <mac1228@gmail.com>
2019-07-24 21:32:44 -0700

    add echo bot

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+'use strict';
+
 var express = require('express');
 var path = require("path");
+var bot = require('./bot');
 
 var app = new express();
 
@@ -10,6 +13,8 @@ app.use(express.static(__dirname + '/../dist/'));
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/../dist/app.html'));
 });
+
+bot.setup(app);
 
 app.listen(port, function() {
     console.log(`App started listening on port ${port}`);
