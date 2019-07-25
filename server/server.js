9d6e15fca6eb3a23cc996a5a22b627b397c7ceba
Mike Curd <mac1228@gmail.com>
2019-07-24 18:51:02 -0700

    remove sample

--- server/server.js
+++ server/server.js
@@ -1,22 +1,14 @@
-// var express = require('express');
-// var path = require("path");
+var express = require('express');
+var path = require("path");
 
-// var app = new express();
+var app = new express();
 
-// var port = process.env.PORT || 3333;
+var port = process.env.PORT || 3333;
 
-// app.use(express.static(__dirname + '/../dist/'));
+app.use(express.static(__dirname + '/../dist/'));
 
-// app.get('/', function(req, res){
-//     res.sendFile(path.join(__dirname + '/../dist/app.html'));
-// }).listen(port, function() {
-//     console.log(`App started listening on port ${port}`);
-// });
-
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3333;
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/../dist/app.html'));
+}).listen(port, function() {
+    console.log(`App started listening on port ${port}`);
+});
