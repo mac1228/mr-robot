58cef481f35975c82a7144e1a397364b38f7c7bd
Mike Curd <mac1228@gmail.com>
2019-07-24 20:22:17 -0700

    + scripts (watch is broken). assets. new manifest.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
-let {dest, src, parallel } = require('gulp');
-let browserify = require('browserify');
-let source = require('vinyl-source-stream');
-let watchify = require('watchify');
-let tsify = require('tsify');
-let sass = require('gulp-sass');
-let concat = require('gulp-concat');
-let autoprefixer = require('gulp-autoprefixer');
+const {dest, src, parallel, series, watch } = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const watchify = require('watchify');
+const tsify = require('tsify');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const LiveServer = require('gulp-live-server');
+const browserSync = require('browser-sync');
+const babelify = require('babelify');
 
-let path = {
+const path = {
   SRC_HTML: 'app/**/*.html',
   SRC_TSX: 'app/**/*.tsx',
   SRC_SCSS: 'app/**/*.scss',
@@ -17,7 +20,7 @@ let path = {
   ENTRY_POINT: 'app/app.tsx'
 };
 
-let b = browserify({
+const b = browserify({
   entries: path.ENTRY_POINT,
   debug: true,
   cache: {},
@@ -25,12 +28,12 @@ let b = browserify({
   plugin: [watchify, tsify]
 });
 
-let buildHtml = function(){
+const buildHtml = () => {
   return src(path.SRC_HTML)
   .pipe(dest(path.DIST_DIR));
 };
 
-let buildCss = function(){
+const buildCss = () => {
   return src(path.SRC_SCSS)
   .pipe(concat(path.OUT_CSS))
   .pipe(sass().on('error', sass.logError))
@@ -38,7 +41,7 @@ let buildCss = function(){
   .pipe(dest(path.DIST_DIR));
 };
 
-let buildJs = function(){
+const buildJs = () => {
   return b.transform('babelify', {
       presets: ['env'],
       extensions: ['.tsx']
@@ -48,7 +51,33 @@ let buildJs = function(){
   .pipe(dest(path.DIST_DIR));
 };
 
+const serve = () => {
+  const server = new LiveServer('server/server.js');
+  server.start();
+  browserSync.init(null, {
+    proxy:'http://localhost:3333',
+    port: 9001,
+    ghostMode: false
+  });
+};
+
+const startWatchers = () => {
+  watch(path.SRC_TSX, buildJs);
+
+  watch(path.SRC_HTML, buildHtml);
+
+  watch(path.SRC_SCSS, buildCss);
+
+  watch(path.DIST_DIR + '/*', browserSync.reload);
+}
+
 exports.buildHtml = buildHtml();
 exports.buildJs = buildJs();
 exports.buildCss = buildCss();
-exports.default = parallel(buildHtml, buildCss, buildJs);
+
+const buildAll = parallel(buildHtml, buildCss, buildJs);
+
+exports.default = buildAll;
+
+exports.serve = series(buildAll, serve);
+exports.watch = series(buildAll, serve, startWatchers);
